79f2495de14874be031abaf6820668ad55ba942c
vllacuna <vllacuna@csumb.edu>
2026-03-24 11:41:31 -0700

    Added members, constructor, setters, getters, hashcode, toString, and equals

--- src/main/java/Utilities/Reader.java
+++ src/main/java/Utilities/Reader.java
@@ -1,5 +1,78 @@
 package Utilities;
 
-public class Reader {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+public class Reader {
+    public static final int CARD_NUMBER_ = 0;
+    public static final int NAME_ = 1;
+    public static final int PHONE_ = 2;
+    public static final int BOOK_COUNT_ = 3;
+    public static final int BOOK_START_ = 4;
+
+    private int cardNumber;
+    private String name;
+    private String phone;
+    private List<Book> books;
+
+    public Reader(int cardNumber, String name, String phone) {
+        this.cardNumber = cardNumber;
+        this.name = name;
+        this.phone = phone;
+        books = new ArrayList<>;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public int getBookCount() {
+        return books.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Reader reader = (Reader) o;
+        return cardNumber == reader.cardNumber && Objects.equals(name, reader.name) && Objects.equals(phone, reader.phone) && Objects.equals(books, reader.books);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardNumber, name, phone, books);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (#" + cardNumber + ") has checked out " + books.toString().replace("[", "{").replace("]", "}");
+    }
 }
