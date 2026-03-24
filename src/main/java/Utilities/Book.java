3208c81bf16d0b50d223ded56b555e2238295f6d
vllacuna <vllacuna@csumb.edu>
2026-03-24 10:48:14 -0700

    UML Constructor, Fields, Methods created

--- src/main/java/Utilities/Book.java
+++ src/main/java/Utilities/Book.java
@@ -1,4 +1,95 @@
 package Utilities;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Book {
+    public static final int IBSN_ = 0;
+    public static final int TITLE_ = 1;
+    public static final int SUBJECT_ = 2;
+    public static final int PAGE_COUNT_ = 3;
+    public static final int AUTHOR_ = 4;
+    public static final int DUE_DATE_ = 5;
+
+    private String isbn;
+    private String title;
+    private String subject;
+    private String author;
+    private LocalDate dueDate;
+    private int pageCount;
+
+
+    public Book(String isbn, String title, String subject, String author, LocalDate dueDate, int pageCount) {
+        this.isbn = isbn;
+        this.title = title;
+        this.subject = subject;
+        this.author = author;
+        this.dueDate = dueDate;
+        this.pageCount = pageCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return pageCount == book.pageCount && Objects.equals(isbn, book.isbn) && Objects.equals(title, book.title) && Objects.equals(subject, book.subject) && Objects.equals(author, book.author) && Objects.equals(dueDate, book.dueDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn, title, subject, author, dueDate, pageCount);
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + author + " ISBN: " + isbn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
 }
