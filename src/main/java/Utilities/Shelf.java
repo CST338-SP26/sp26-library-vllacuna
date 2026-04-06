442964e9bad82221c2a419a19e4e9b689fd702ae
vllacuna <vllacuna@csumb.edu>
2026-04-06 01:35:42 -0700

    Fixed code to pass tests, added header and javadoc comments

--- src/main/java/Utilities/Shelf.java
+++ src/main/java/Utilities/Shelf.java
@@ -3,6 +3,13 @@ package Utilities;
 import java.util.HashMap;
 import java.util.Objects;
 
+/**
+ * Name: Von Andre Llacuna
+ * Date: 04/05/26
+ * FileName: Shelf.java
+ * AssignmentName: Project 01 Part 03/04: Shelf.java
+ * Description: A file that represents a shelf in the larger library project
+ */
 public class Shelf {
     public static final int SHELF_NUMBER = 0;
     public static final int SUBJECT_ = 1;
@@ -20,6 +27,11 @@ public class Shelf {
         books = new HashMap<>();
     }
 
+    /**
+     * gets the book count
+     * @param book represents book
+     * @return the book count, -1 if not found
+     */
     public int getBookCount(Book book) {
         if (!books.containsKey(book)) {
             return -1;
@@ -28,23 +40,43 @@ public class Shelf {
         }
     }
 
+    /**
+     * adds parameter 'book' to 'books' HashMap of the shelf
+     * @param book represents the book trying to be added to the HashMap
+     * @return a SUCCESS code if added and a shelf subject mismatch code
+     * if the parameter 'book' subject doesn't match the shelf subject
+     */
     public Code addBook(Book book) {
         if (!subject.equals(book.getSubject())) {
             return Code.SHELF_SUBJECT_MISMATCH_ERROR;
-        } else if (books.containsKey(book)) {
-            Integer temporary = books.get(book);
-            temporary++;
-            books.put(book, temporary);
+        } else if (books == null) {
+            books = new HashMap<>();
+            books.put(book, 1);
+            System.out.println(book + " added to shelf");
+            return Code.SUCCESS;
+        } else if (books.containsKey(book)){
+            Integer temp = books.get(book);
+            temp++;
+            books.put(book, temp);
             return Code.SUCCESS;
         } else {
             books.put(book, 1);
-            System.out.println(book + "added to shelf");
+            System.out.println(book + " added to shelf");
             return Code.SUCCESS;
         }
     }
 
+    /**
+     * removes a book from the shelf
+     * @param book represents the book trying to be removed
+     * @return SUCCESS code if book was successfully removed and
+     * not in inventory code if book isn't on the shelf
+     */
     public Code removeBook(Book book) {
-        if (!books.containsKey(book)) {
+        if (books == null) {
+            System.out.println(book.getTitle() + " is not on the shelf " + subject);
+            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        } else if (!books.containsKey(book)) {
             System.out.println(book.getTitle() + " is not on the shelf " + subject);
             return Code.BOOK_NOT_IN_INVENTORY_ERROR;
         } else if (books.containsKey(book) && (books.get(book) == 0)) {
@@ -57,8 +89,15 @@ public class Shelf {
         }
     }
 
+    /**
+     * Shows how many books are on the shelf and which books
+     * @return the string listing of all the books on the shelf
+     */
     public String listBooks() {
         StringBuilder sb = new StringBuilder();
+        if (books == null || books.isEmpty()) {
+            return "0 books on shelf: " + shelfNumber + " : " + subject;
+        }
         if(books.size() == 1){
             sb.append(books.size()).append(" book on shelf: ").append(shelfNumber).append(" : ").append(subject).append("\n");
         } else {
